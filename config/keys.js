24a57876ac5ccfeab857736ef1bd15b27a4e1e05
abdougenesis <fa_djellal@esi.dz>
2019-10-31 21:14:32 +0100

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// keys.js -- figure credentials to return
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+mongoose.connection
+  .once("open", () => {
+    console.log("oppened");
+  })
+  .on("error", err => {
+    console.log("mongo error");
+    console.log(err);
+  });
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "fucker" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
